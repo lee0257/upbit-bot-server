cb35f26efd59474c419eae009f773dd750ca2f2a
lee0257 <lee45230257@gmail.com>
2025-04-29 14:33:33 +0900

    Update server_manager.py

--- server_manager.py
+++ server_manager.py
@@ -1,1 +1,11 @@
-# 메인 서버 실행 파일 (예시)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "업비트 봇 서버 실행 중!"
+
+if __name__ == "__main__":
+    print("서버 매니저가 실행되었습니다.")
+    app.run(host="0.0.0.0", port=8000)
